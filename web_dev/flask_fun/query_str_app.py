b2a2518828c110eb987a78c95049f9e7a5c4f0ee
Nick Tallant <ndtallant@uchicago.edu>
2018-03-17 09:59:42 -0500

    Fun with awk and flask

--- web_dev/flask_fun/query_str_app.py
+++ web_dev/flask_fun/query_str_app.py
@@ -8,3 +8,8 @@ app.debug = True
 @app.route('/<name>')
 def index(name='Nick'):
     return 'Hello, {}'.format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    context = {'num1': num1, 'num2': num2}
+    return render_template('add.html', **context)
