226fc8a260f9a076d76f8dd626ad0b59f2c49f17
Nick Tallant <ndtallant@uchicago.edu>
2020-02-02 09:47:30 -0600

    Plotted some models

--- regression/pooling.R
+++ regression/pooling.R
@@ -25,3 +25,29 @@ summary(pooled)
 # No Pooling, no intercept term.
 no.pool <- lm(y ~ x + county - 1, df)
 summary(no.pool)
+
+# Plot County Estimates from No Pooled Model
+broom::tidy(no.pool) %>%
+  filter(term != "x") %>%
+  arrange(estimate) %>%
+  mutate(x = row_number()) %>%
+    ggplot(aes(x, estimate)) +
+      geom_point() +
+      geom_errorbar(aes(ymin=(estimate - std.error),
+                    ymax=(estimate + std.error)))
+
+# Partially Pooled
+p.pool <- stan_glmer(y ~ x + (1 | county), data = df)
+p.pool.est <- as_tibble(p.pool) %>%
+  select(contains("b["))
+
+m.est <- as_tibble(t(map_df(p.pool.est, mean)), name_repair = "min")
+sd.est <- as_tibble(t(map_df(p.pool.est, sd)), name_repair = "min")
+bind_cols(c(m.est, sd.est)) %>%
+  select(estimate = V1, std.error = V11) %>%
+  arrange(estimate) %>%
+  mutate(x = row_number()) %>%
+    ggplot(aes(x, estimate)) +
+      geom_point() +
+      geom_errorbar(aes(ymin=(estimate - std.error),
+                    ymax=(estimate + std.error)))
